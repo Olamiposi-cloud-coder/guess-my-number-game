c48d7da8036ea7ff8f0a5bac9fc2f333d071f20c
Olamiposi <olamiposicloud25@gmail.com>
2024-03-27 21:13:25 +0100

    new update on js

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 const number = document.querySelector('.number');
 const message = document.querySelector('.message');
 const guessInput = document.querySelector('.guess');
 const btn = document.querySelector('.check');
 const score = document.querySelector('.score');
+const body = document.querySelector('body');
+const reload = document.querySelector('.again');
+const highScore = document.querySelector('.highscore');
+
 let scoreOutput = 20;
+let highScoreOutput = 0;
+
+const displayMessage = function (text) {
+  message.textContent = text;
+};
 
 btn.addEventListener('click', () => {
   const guess = Number(guessInput.value);
-  number.textContent = secretNumber;
+
   console.log(guess, typeof guess);
 
   if (!guess) {
-    message.textContent = '⛔⛔No number provided!!!';
+    displayMessage('⛔⛔No number provided!!!');
   } else if (guess === secretNumber) {
-    message.textContent = '📌📌Correct Number!!!';
+    displayMessage('📌📌Correct Number!!!');
     scoreOutput++;
     score.textContent = scoreOutput;
-  } else if (guess > secretNumber) {
-    if (scoreOutput > 1) {
-      message.textContent = '😞😞Number Too High ';
-      scoreOutput--;
-      score.textContent = scoreOutput;
-    } else {
-      message.textContent = 'You lost the game😞😞';
-      score.textContent = 0;
+    number.textContent = secretNumber;
+
+    body.style.backgroundColor = '#60b347';
+    number.style.width = '30rem';
+
+    if (scoreOutput > highScoreOutput) {
+      highScoreOutput = scoreOutput;
+      highScore.textContent = highScoreOutput;
     }
-  } else if (guess < secretNumber) {
+  } else if (guess !== secretNumber) {
     if (scoreOutput > 1) {
-      message.textContent = '😞😞Number Too Low ';
+      displayMessage(
+        guess > secretNumber ? '😞😞Number Too High ' : '😞😞Number Too Low'
+      );
       scoreOutput--;
       score.textContent = scoreOutput;
     } else {
-      message.textContent = 'You lost the game😞😞';
+      displayMessage('You lost the game😞😞');
       score.textContent = 0;
     }
   }
+});
+
+reload.addEventListener('click', function () {
+  scoreOutput = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  number.textContent = '?';
+  displayMessage('Start guessing...');
+  score.textContent = scoreOutput;
+  guessInput.value = '';
+  body.style.backgroundColor = '#222';
+  number.style.width = '15rem';
 });
