0f55298ebd60997cf3f0b6c5dea61f48c4b21dd9
Olamiposi <olamiposicloud25@gmail.com>
2024-03-27 15:54:44 +0100

    starting js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const guessInput = document.querySelector('.guess');
+const btn = document.querySelector('.check');
+btn.addEventListener('click', () => {
+  console.log(guessInput.value);
+});
